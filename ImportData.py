698667f035f4bc66278b8ae3ba7ed67971609be7
Sorgl0s <tristantcooper@gmail.com>
2021-04-26 20:17:50 +1000

    ImportData now also imports test data.

--- ImportData.py
+++ ImportData.py
@@ -5,44 +5,60 @@ import numpy as np
 import pickle
 
 def import_data(modes):
-    data = {}
+    data = {"test": {}, "train": {}}
+    test = pd.read_csv("COMP30027_2021_Project2_datasets/recipe_test.csv")
     train = pd.read_csv("COMP30027_2021_Project2_datasets/recipe_train.csv")
     data["duration"] = train["duration_label"]
 
     # Pandas series
     if "n_steps" in modes:
-        data["n_steps"] = train["n_steps"]
+        data[test["n_steps"]] = test["n_steps"]
+        data[train["n_steps"]] = train["n_steps"]
     
     # Pandas series
     if "n_ingr" in modes:
-        data["n_ingr"] = train["n_ingredients"]
+        data[test["n_ingr"]] = test["n_ingredients"]
+        data[train["n_ingr"]] = train["n_ingredients"]
 
-    # data[countvec]: List of dictionaries of wordcounts per instance. 
-    # data[BoW]: List of vectors of wordcounts per instance. Vectors are each the size of #unique words from all instances.
+    # data[train[countvec]]: List of dictionaries of wordcounts per instance for training set. 
+    # data[set[BoW]]: List of vectors of wordcounts per instance. Vectors are each the size of #unique words
+    # from all instances of the respective dataset.
     # List dimensions have: 0: name, 1: ingredients, 2: steps.
     if "countvec" in modes:
         directory = "COMP30027_2021_Project2_datasets/recipe_text_features_countvec/"
         file_names = ("train_name_countvectorizer.pkl", "train_ingr_countvectorizer.pkl", "train_steps_countvectorizer.pkl")
-        dicts = [pickle.load(open(directory + file_name, "rb")).vocabulary_ for file_name in file_names]
+        count_dicts = [pickle.load(open(directory + file_name, "rb")).vocabulary_ for file_name in file_names]
 
-        file_names = ("train_name_vec.npz", "train_ingr_vec.npz", "train_steps_vec.npz")
-        BoWmatrices = [scipy.sparse.load_npz(directory + file_name) for file_name in file_names]
-        data["countvec"] = dicts
-        data["BoW"] = BoWmatrices
+        test_file_names = ("test_name_vec.npz", "test_ingr_vec.npz", "test_steps_vec.npz")
+        train_file_names = ("train_name_vec.npz", "train_ingr_vec.npz", "train_steps_vec.npz")
+
+        test_BoW_matrices = [scipy.sparse.load_npz(directory + file_name) for file_name in test_file_names]
+        train_BoW_matrices = [scipy.sparse.load_npz(directory + file_name) for file_name in train_file_names]
+        data[train["countvec"]] = count_dicts
+        data[test["BoW"]] = test_BoW_matrices
+        data[train["BoW"]] = train_BoW_matrices
     
-    # data[doc50]: List of pandas dataframes each with a 50 dimensional vector per instance. 
+    # data[set[doc50]]: List of pandas dataframes each with a 50 dimensional vector per instance. 
     # List dimensions have: 0: name, 1: ingredients, 2: steps.
     if "doc50" in modes:
         directory = "COMP30027_2021_Project2_datasets/recipe_text_features_doc2vec50/"
-        file_names = ("train_name_doc2vec50.csv", "train_ingr_doc2vec50.csv", "train_steps_doc2vec50.csv")
-        doc50 = [pd.read_csv(directory + file_name, index_col = False, delimiter = ',', header=None) for file_name in file_names]
-        data["doc50"] = doc50
+        test_file_names = ("test_name_doc2vec50.csv", "test_ingr_doc2vec50.csv", "test_steps_doc2vec50.csv")
+        train_file_names = ("train_name_doc2vec50.csv", "train_ingr_doc2vec50.csv", "train_steps_doc2vec50.csv")
+        test_doc50 = [pd.read_csv(directory + file_name, index_col = False, delimiter = ',', header=None) for file_name in test_file_names]
+        train_doc50 = [pd.read_csv(directory + file_name, index_col = False, delimiter = ',', header=None) for file_name in train_file_names]
+        data[test["doc50"]] = test_doc50
+        data[train["doc50"]] = train_doc50
     
-    # data[doc100]: List of pandas dataframes each with a 100 dimensional vector per instance. 
+    # data[set[doc100]]: List of pandas dataframes each with a 100 dimensional vector per instance. 
     # List dimensions have: 0: name, 1: ingredients, 2: steps.
     if "doc100" in modes:
         directory = "COMP30027_2021_Project2_datasets/recipe_text_features_doc2vec100/"
-        file_names = ("train_name_doc2vec100.csv", "train_ingr_doc2vec100.csv", "train_steps_doc2vec100.csv")
-        doc100 = [pd.read_csv(directory + file_name, index_col = False, delimiter = ',', header=None) for file_name in file_names]
-        data["doc100"] = doc100
+        test_file_names = ("test_name_doc2vec100.csv", "test_ingr_doc2vec100.csv", "test_steps_doc2vec100.csv")
+        train_file_names = ("train_name_doc2vec100.csv", "train_ingr_doc2vec100.csv", "train_steps_doc2vec100.csv")
+        test_doc100 = [pd.read_csv(directory + file_name, index_col = False, delimiter = ',', header=None) for file_name in test_file_names]
+        train_doc100 = [pd.read_csv(directory + file_name, index_col = False, delimiter = ',', header=None) for file_name in train_file_names]
+        data[test["doc100"]] = test_doc100
+        data[train["doc100"]] = train_doc100
     return data
+
+    import_data(["countvec", "doc50", "doc100"])
