78484ee31b7d555090181ed6f36a7e5af969f483
Sorgl0s <tristantcooper@gmail.com>
2021-05-05 20:23:39 +1000

    SVMv1: Now compatible with train-test-split.

--- SVMv1.py
+++ SVMv1.py
@@ -5,19 +5,20 @@ import numpy as np
 
 from ImportData import import_data
 data = import_data("doc100")
-n_samples = len(data["train"]["duration"])
 
-X = pd.concat(data["train"]["doc100"], axis = 1).to_numpy()
-y = data["train"]["duration"].to_numpy()
+X_train = pd.concat(data["train"]["train"]["doc100"], axis = 1).to_numpy()
+y_train = data["train"]["train"]["duration"].to_numpy()
+X_test = pd.concat(data["train"]["test"]["doc100"], axis = 1).to_numpy()
+y_test = data["train"]["test"]["duration"].to_numpy()
 
 with parallel_backend('threading', n_jobs=8):
-	model = svm.SVC(verbose=True, max_iter = 5000)
-	model.fit(X,y)
-	model.score(X,y)
+	model = svm.SVC(verbose=True, max_iter = 50000)
+	model.fit(X_train,y_train)
+	model.score(X_test,y_test)
 	
 # 0.72 with 5000 iterations
 
 def load_model_and_score():
 	from joblib import load
 	model = load("svm_model.sav")
-	model.score(X,y)
+	model.score(X_test,y_test)
