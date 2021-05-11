bec99a527703e7a5404d371bdd546450e4fccf0d
Sorgl0s <tristantcooper@gmail.com>
2021-05-11 13:26:54 +1000

    Updated KNN with testing history. 0-R now prints most frequent label.

--- 0-R.py
+++ 0-R.py
@@ -5,12 +5,14 @@ from ImportData import import_data
 def zeroR_predict(train_labels, n):
 	counts = Counter(train_labels)
 	most_frequent = counts.most_common(1)[0][0]
+	print("Most frequent label: ", most_frequent)
 	return [most_frequent] * n
 
 def zeroR_evaluate(train_labels, test_labels):
 	predictions = zeroR_predict(train_labels, len(test_labels))
 	return 100*sum(pred == label for pred, label in zip(predictions, test_labels))/len(test_labels)
 
-def baseline( test_size = 0.8, RS = 1):
-	data = import_data(["0-R"], test_size, RS)
+def baseline(train_size = 0.8, RS = 1):
+	data = import_data(["0-R"], train_size, RS)
 	return zeroR_evaluate(data[0], data[1])
+	# Default params give 2.0 with 49% acc.
--- KNN.py
+++ KNN.py
@@ -7,10 +7,12 @@ import matplotlib.pyplot as plt
 
 from ImportData import import_data
 
-data = import_data(['doc50'])
+data = import_data(["0"])
 
-data_train = [pd.concat(data["train"]["train"]["doc50"], axis = 1)]#, data["train"]["train"]["n_ingr"], data["train"]["train"]["n_steps"]]
-data_test = [pd.concat(data["train"]["test"]["doc50"], axis = 1)]#, data["train"]["test"]["n_ingr"], data["train"]["test"]["n_steps"]]
+#data_train = [pd.concat(data["train"]["train"]["doc50"], axis = 1), data["train"]["train"]["n_ingr"], data["train"]["train"]["n_steps"]]
+#data_test = [pd.concat(data["train"]["test"]["doc50"], axis = 1), data["train"]["test"]["n_ingr"], data["train"]["test"]["n_steps"]]
+data_train = [data["train"]["train"]["n_ingr"], data["train"]["train"]["n_steps"]]
+data_test = [data["train"]["test"]["n_ingr"], data["train"]["test"]["n_steps"]]
 #X = pd.concat([data["train"]["train"]["n_ingr"], data["train"]["train"]["n_steps"]], axis = 1)
 #y = data["train"]["train"]["duration"]
 #data_train = [pd.concat(data["train"]["train"]["BoW"], axis = 1)]
@@ -40,3 +42,6 @@ plt.show()
 # 68.3625% with 1.5^9 neighbours d50 steps
 # 64% d50 ingr
 # 69.325% with 1.5^10 neighbours d50 ingr steps weighted
+# 66.3344% with 1.5^25 (?) d50 weighted
+# 70.2875% with k=1.5^10 d50 ingr steps
+# 63.4313% with k=1.5^13 ingr steps
