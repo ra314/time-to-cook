09f24cbd4eb0d178c277a630377a845a4a282ad4
Rahul Gandharva <rahulgandharva20122000@gmail.com>
2021-05-12 15:43:03 +1000

    Implemented train test split in logistic regression.

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -3,18 +3,24 @@ import pandas as pd
 import numpy as np
 
 from ImportData import import_data
-data = import_data("none")
-n_samples = len(data["train"]["duration"])
-X1 = data["train"]["n_steps"].to_numpy().reshape((n_samples,1))
-X2 = data["train"]["n_ingr"].to_numpy().reshape((n_samples,1))
-X = np.stack((X1,X2), axis =1).reshape((n_samples,2))
-y = data["train"]["duration"].to_numpy()
+data = import_data("doc100")
+n_samples = len(data["train"]["train"]["duration"])
+X1 = data["train"]["train"]["n_steps"].to_numpy().reshape((n_samples,1))
+X2 = data["train"]["train"]["n_ingr"].to_numpy().reshape((n_samples,1))
+X0 = np.stack((X1,X2), axis =1).reshape((n_samples,2))
+y1 = data["train"]["train"]["duration"].to_numpy()
+
+n_samples = len(data["train"]["test"]["duration"])
+W1 = data["train"]["test"]["n_steps"].to_numpy().reshape((n_samples,1))
+W2 = data["train"]["test"]["n_ingr"].to_numpy().reshape((n_samples,1))
+W0 = np.stack((W1,W2), axis =1).reshape((n_samples,2))
+y2 = data["train"]["test"]["duration"].to_numpy()
 
 models = {}
-models["n_steps & n_ingr"] = (LogisticRegression(random_state=0).fit(X, y), X)
-models["n_steps"] = (LogisticRegression(random_state=0).fit(X1, y), X1)
-models["n_ingr"] = (LogisticRegression(random_state=0).fit(X2, y), X2)
+models["n_steps & n_ingr"] = (LogisticRegression(random_state=0).fit(X0, y1), X0, y1, W0, y2)
+models["n_steps"] = (LogisticRegression(random_state=0).fit(X1, y1), X1, y1, W1, y2)
+models["n_ingr"] = (LogisticRegression(random_state=0).fit(X2, y1), X2, y1, W2, y2)
 
 for model_name, model_and_data in models.items():
-	model, data = model_and_data
-	print(f"Logistic regression with {model_name}: {model.score(data, y)*100:.2f}% acc")
+	model, X, y1, W, y2 = model_and_data
+	print(f"Logistic regression with {model_name}: {model.score(X, y1)*100:.2f}% training acc {model.score(W, y2)*100:.2f}% test acc")
