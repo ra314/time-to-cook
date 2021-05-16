0455d1ec85ed36486fbe5ccd53f203bf4f3b58a1
Sorgl0s <tristantcooper@gmail.com>
2021-05-16 22:30:35 +1000

    LSTM and GRU testing history.

--- LSTM+GRU+RNN.py
+++ LSTM+GRU+RNN.py
@@ -7,12 +7,14 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from keras.models import Model, Sequential
-from keras.layers import LSTM, Activation, Dense, Dropout, Input, Embedding, SpatialDropout1D
+from keras.layers import LSTM, Activation, Dense, Dropout, Input, Embedding, SpatialDropout1D, GRU, Bidirectional
 from keras.optimizers import RMSprop
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing import sequence
 from keras.callbacks import EarlyStopping
-CUDA_VISIBLE_DEVICES=""
+
+#import os
+#os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
 from ULMFiT import format_to_text
 
@@ -56,7 +58,9 @@ def RNN():
 model = Sequential()
 model.add(Embedding(max_words, 50, input_length=max_len))
 model.add(SpatialDropout1D(0.2))
-model.add(LSTM(100, dropout=0.2, recurrent_dropout=0.2))
+#model.add(GRU(200, return_sequences=True, dropout=0.2, recurrent_dropout=0))
+model.add(Bidirectional(GRU(200, return_sequences=True, dropout=0.2, recurrent_dropout=0)))
+model.add(Bidirectional(GRU(200, dropout=0.2, recurrent_dropout=0)))
 model.add(Dense(3, activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
@@ -68,7 +72,7 @@ model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accur
 model.summary()
 model.compile(loss='binary_crossentropy',optimizer=RMSprop(),metrics=['accuracy'])
 
-model.fit(sequences_matrix,Y_train,batch_size=1024,epochs=10, validation_split=0.2,callbacks=[EarlyStopping(monitor='val_loss',min_delta=0.0001)])
+model.fit(sequences_matrix,Y_train,batch_size=256,epochs=30, validation_split=0.2,callbacks=[EarlyStopping(monitor='val_loss', patience = 5, min_delta=0.0001)])
           
 test_sequences = tok.texts_to_sequences(X_test) 
 test_sequences_matrix = sequence.pad_sequences(test_sequences,maxlen=max_len)
@@ -76,3 +80,31 @@ test_sequences_matrix = sequence.pad_sequences(test_sequences,maxlen=max_len)
 accr = model.evaluate(test_sequences_matrix,Y_test)
 
 print('Test set\n  Loss: {:0.3f}\n  Accuracy: {:0.3f}'.format(accr[0],accr[1]))
+
+# LSTM 100 neurons softmax no rec dropout 44.1% acc on 0.15 test size 0.0001 callback
+# LSTM 500 neurons softmax no rec dropout 66.9% acc on 0.15 test size 0.0001 callback
+# LSTM 10 neurons softmax no rec dropout 72.4% acc on 0.15 test size 0.0001 callback
+# LSTM 5 neurons softmax no rec dropout 50.0% acc on 0.15 test size 0.0001 callback
+# GRU 50 neurons softmax no rec dropout 67.6% acc on 0.15 test size 0.0001 callback
+# GRU 50 neurons softmax no rec dropout 72.5% acc on 0.15 test size 0.0005 callback
+# GRU 50 neurons softmax no rec dropout 71.1% acc on 0.15 test size 0.01 callback
+# LSTM 100 neurons softmax rec dropout = 0.1 51.0% acc on 0.15 test size 0.0001 callback
+# LSTM 100 neurons softmax rec dropout = 0 76.0% acc on 0.15 test size 0.0001 callback patience = 3
+# LSTM 200 neurons softmax rec dropout = 0 74.7% acc on 0.15 test size 0.0001 callback patience = 3
+# LSTM 200 neurons softmax rec dropout = 0 78.0% acc on 0.15 test size 0.0001 callback patience = 3 15 epochs
+# LSTM 200 + 200 neurons softmax rec dropout = 0 76.5% acc on 0.15 test size 0.0001 callback patience = 3 10 epochs
+#   Adding another 10 epochs: 78.0%, did not finish early.
+#   Adding another 10 epochs: 78.4%. Ended early.
+# LSTM 200 + 200 + 200 neurons softmax rec dropout = 0 78.8% acc on 0.15 test size 0.0001 callback patience = 3 10 epochs did not finish early.
+#   Adding another 20 epochs: Stopped after 6. 77.9%
+#   Adding another 20 epochs: Stopped after 10. 79.8% 
+# GRU 200 neurons softmax rec dropout = 0 78.1% acc on 0.15 test size 0.0001 callback patience = 5 20 epochs did not finish early.
+#    Adding another 20 epochs: Stopped after 12. 79.2%
+# GRU 200 + 200 neurons softmax rec dropout = 0 79.8% acc on 0.15 test size 0.0001 callback patience = 5 20 epochs finished at 17.
+#   Adding another 20 epochs: Stopped after 14. 79.8%.
+#   Adding another 20 epochs: Stopped after 6: 79.4%
+# BID LSTM 200 neurons softmax rec dropout = 0 78.0% acc on 0.15 test size 0.0001 callback patience = 5 20 epochs did not finish early.
+# BID GRU 200 neurons softmax rec dropout = 0 75.9% acc on 0.15 test size 0.0001 callback patience = 5 20 epochs finished at 19. Promising around 15 epochs.
+# BID LSTM 200 + 200 neurons softmax rec dropout = 0 79.8% acc on 0.15 test size 0.0001 callback patience = 5 30 epochs finished at 24.
+# BID GRU 200 + 200 neurons softmax rec dropout = 0 80.3% acc on 0.15 test size 0.0001 callback patience = 5 30 epochs finished at 13. Not sure why it stopped though.
+#   Another 30 epochs: Stopped after 7. 79.0%.
