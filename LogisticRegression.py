40d71dc8deebc54c9bdb525007fc808c375b7d3b
Rahul Gandharva <rahulgandharva20122000@gmail.com>
2021-05-16 13:25:23 +1000

    Applied lofgistic regression on doc2vec.

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -24,3 +24,14 @@ models["n_ingr"] = (LogisticRegression(random_state=0).fit(X2, y1), X2, y1, W2, 
 for model_name, model_and_data in models.items():
 	model, X, y1, W, y2 = model_and_data
 	print(f"Logistic regression with {model_name}: {model.score(X, y1)*100:.2f}% training acc {model.score(W, y2)*100:.2f}% test acc")
+	
+#Logistic regression on doc100
+X_train = pd.concat(data["train"]["train"]["doc100"], axis = 1)
+X_test = pd.concat(data["train"]["test"]["doc100"], axis = 1)
+
+model = LogisticRegression(random_state=0, max_iter=1000).fit(X_train, y1)
+print(f"Logistic regression with doc100: {model.score(X_train, y1)*100:.2f}% training acc {model.score(X_test, y2)*100:.2f}% test acc")
+
+from sklearn.ensemble import BaggingClassifier
+model = BaggingClassifier(base_estimator=LogisticRegression(random_state=0, max_iter=1000), n_estimators=10, random_state=0).fit(X_train, y1)
+print(f"Logistic regression with doc100 and boosting: {model.score(X_train, y1)*100:.2f}% training acc {model.score(X_test, y2)*100:.2f}% test acc")
