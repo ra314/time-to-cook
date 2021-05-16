c2d220fa150c792e5b5cbd329b408b011d6f2371
Rahul Gandharva <rahulgandharva20122000@gmail.com>
2021-05-17 09:15:39 +1000

    Added code in NeuralNetworks to run with Keras with deep networks.

--- NeuralNetworks.py
+++ NeuralNetworks.py
@@ -3,17 +3,25 @@ import pandas as pd
 import numpy as np
 from sklearn.neural_network import MLPClassifier
 
+from keras.models import Sequential
+from keras.layers import InputLayer, Dense, Dropout
+from keras.callbacks import EarlyStopping
+
+
 data_type = "doc100"
 data = import_data(data_type)
 
-data_train = [pd.concat(data["train"]["train"][data_type], axis = 1), data["train"]["train"]["n_steps"]]#data["train"]["train"]["n_ingr"]]#, data["train"]["train"]["n_steps"]]
-data_test = [pd.concat(data["train"]["test"][data_type], axis = 1), data["train"]["test"]["n_steps"]]#data["train"]["test"]["n_ingr"]]#, data["train"]["test"]["n_steps"]]
-data_Test = [pd.concat(data["test"][data_type], axis = 1), data["test"]["n_steps"]]# data["test"]["n_ingr"]]#, data["test"]["n_steps"]]
+max_n_steps = max(data["train"]["train"]["n_steps"])
+max_n_ingr = max(data["train"]["train"]["n_ingr"])
+
+data_train = [pd.concat(data["train"]["train"][data_type], axis = 1), data["train"]["train"]["n_ingr"]/max_n_ingr, data["train"]["train"]["n_steps"]/max_n_steps]
+data_test = [pd.concat(data["train"]["test"][data_type], axis = 1), data["train"]["test"]["n_ingr"]/max_n_ingr, data["train"]["test"]["n_steps"]/max_n_steps]
+data_Test = [pd.concat(data["test"][data_type], axis = 1), data["test"]["n_ingr"]/max_n_ingr, data["test"]["n_steps"]/max_n_steps]
 
 X_train = pd.concat(data_train, axis = 1).to_numpy()
-Y_train = data["train"]["train"]["duration"].to_numpy()
+Y_train = pd.get_dummies(data["train"]["train"]["duration"]).values
 X_test = pd.concat(data_test, axis = 1).to_numpy()
-Y_test = data["train"]["test"]["duration"].to_numpy()
+Y_test = pd.get_dummies(data["train"]["test"]["duration"]).values
 
 hidden_X_train = pd.concat(data_Test, axis = 1).to_numpy()
 
@@ -51,3 +59,23 @@ for num_layers in range(1, 50, 5):
 		print(f"{num_layers} layers, {layer_size} layer_size. Acc: {score}")
 '''
 
+
+model = Sequential()
+model.add(InputLayer(302,))
+model.add(Dense(100, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(100, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(100, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(100, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(100, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(100, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(3, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+model.summary()
+model.fit(X_train,Y_train,batch_size=1024,epochs=50, validation_split=0.2,callbacks=[EarlyStopping(monitor='val_loss', patience = 20, min_delta=0.0001)])
