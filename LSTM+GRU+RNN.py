46bbcb62e04b1b57eedd8f3230e0161bf5874727
Rahul Gandharva <rahulgandharva20122000@gmail.com>
2021-05-16 18:07:01 +1000

    Updated the script to actually work now and with good output layer stuff.

--- LSTM+GRU+RNN.py
+++ LSTM+GRU+RNN.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-from keras.models import Model
-from keras.layers import LSTM, Activation, Dense, Dropout, Input, Embedding
+from keras.models import Model, Sequential
+from keras.layers import LSTM, Activation, Dense, Dropout, Input, Embedding, SpatialDropout1D
 from keras.optimizers import RMSprop
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing import sequence
@@ -20,13 +20,16 @@ train, test = import_data(["raw"])
 df = pd.concat([train, test], axis=0)
 df = format_to_text(df)
 X, Y = df['text'], df['duration_label']
+Y = pd.get_dummies(df['duration_label']).values
 
-
-le = LabelEncoder()
-Y = le.fit_transform(Y)
-Y = Y.reshape(-1,1)
+#le = LabelEncoder()
+#Y = le.fit_transform(Y)
+#Y = Y.reshape(-1,1)
 
 X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.15)
+
+#Y_train = np.asarray(Y_train).astype('float32').reshape((-1,1))
+#Y_test = np.asarray(Y_test).astype('float32').reshape((-1,1))
 
 max_words = 1000
 max_len = 150
@@ -35,6 +38,7 @@ tok.fit_on_texts(X_train)
 sequences = tok.texts_to_sequences(X_train)
 sequences_matrix = sequence.pad_sequences(sequences,maxlen=max_len)
 
+'''
 def RNN():
     inputs = Input(name='inputs',shape=[max_len])
     layer = Embedding(max_words,50,input_length=max_len)(inputs)
@@ -42,16 +46,32 @@ def RNN():
     layer = Dense(256,name='FC1')(layer)
     layer = Activation('relu')(layer)
     layer = Dropout(0.5)(layer)
-    layer = Dense(1,name='out_layer')(layer)
-    layer = Activation('sigmoid')(layer)
+    layer = Dense(3,name='out_layer')(layer)
+    layer = Activation('softmax')(layer)
     model = Model(inputs=inputs,outputs=layer)
     return model
+'''
 
-model = RNN()
+
+model = Sequential()
+model.add(Embedding(max_words, 50, input_length=max_len))
+model.add(SpatialDropout1D(0.2))
+model.add(LSTM(100, dropout=0.2, recurrent_dropout=0.2))
+model.add(Dense(3, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+epochs = 5
+batch_size = 64
+
+#history = model.fit(sequences_matrix, Y_train, epochs=epochs, batch_size=batch_size,validation_split=0.1,callbacks=[EarlyStopping(monitor='val_loss', patience=3, min_delta=0.0001)])
+
+
+
+#model = RNN()
 model.summary()
 model.compile(loss='binary_crossentropy',optimizer=RMSprop(),metrics=['accuracy'])
 
-model.fit(sequences_matrix,Y_train,batch_size=128,epochs=10, validation_split=0.2,callbacks=[EarlyStopping(monitor='val_loss',min_delta=0.0001)])
+model.fit(sequences_matrix,Y_train,batch_size=1024,epochs=10, validation_split=0.2,callbacks=[EarlyStopping(monitor='val_loss',min_delta=0.0001)])
           
 test_sequences = tok.texts_to_sequences(X_test) 
 test_sequences_matrix = sequence.pad_sequences(test_sequences,maxlen=max_len)
