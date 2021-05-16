0a784a2b788952f6fb18650a9ac92a13440ee689
Rahul Gandharva <rahulgandharva20122000@gmail.com>
2021-05-16 18:21:46 +1000

    Changed neural networks to only scale the X data.

--- NeuralNetworks.py
+++ NeuralNetworks.py
@@ -3,11 +3,12 @@ import pandas as pd
 import numpy as np
 from sklearn.neural_network import MLPClassifier
 
-data = import_data("doc100")
+data_type = "doc100"
+data = import_data(data_type)
 
-data_train = [pd.concat(data["train"]["train"]["doc100"], axis = 1), data["train"]["train"]["n_steps"]]#data["train"]["train"]["n_ingr"]]#, data["train"]["train"]["n_steps"]]
-data_test = [pd.concat(data["train"]["test"]["doc100"], axis = 1), data["train"]["test"]["n_steps"]]#data["train"]["test"]["n_ingr"]]#, data["train"]["test"]["n_steps"]]
-data_Test = [pd.concat(data["test"]["doc100"], axis = 1), data["test"]["n_steps"]]# data["test"]["n_ingr"]]#, data["test"]["n_steps"]]
+data_train = [pd.concat(data["train"]["train"][data_type], axis = 1), data["train"]["train"]["n_steps"]]#data["train"]["train"]["n_ingr"]]#, data["train"]["train"]["n_steps"]]
+data_test = [pd.concat(data["train"]["test"][data_type], axis = 1), data["train"]["test"]["n_steps"]]#data["train"]["test"]["n_ingr"]]#, data["train"]["test"]["n_steps"]]
+data_Test = [pd.concat(data["test"][data_type], axis = 1), data["test"]["n_steps"]]# data["test"]["n_ingr"]]#, data["test"]["n_steps"]]
 
 X_train = pd.concat(data_train, axis = 1).to_numpy()
 Y_train = data["train"]["train"]["duration"].to_numpy()
@@ -22,16 +23,14 @@ from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
 scaler.fit(X_train)
 X_train = scaler.transform(X_train)
-Y_train = scaler.transform(Y_train)
-y_train = scaler.transform(hidden_X_train)
+X_test = scaler.transform(X_test)
+hidden_X_train = scaler.transform(hidden_X_train)
 
-clf = MLPClassifier(solver='adam', alpha=1e-5, hidden_layer_sizes=(50, 50, 50, 50), random_state=1, max_iter = 100)
+clf = MLPClassifier(solver='adam', alpha=1e-5, hidden_layer_sizes=(3,3), random_state=1, max_iter = 10)
 clf.fit(X_train, Y_train)
 clf.score(X_test, Y_test)
 
-clf.partial_fit(X_train, Y_train, classes = np.array([1,2,3]))
-
-#With 3,3 and 10 iterations you can get 0.7135 acc.
+#With 3,3 and 10 iterations you can get 0.7135 acc. (doc100)
 '''
 
 '''
@@ -51,8 +50,4 @@ for num_layers in range(1, 50, 5):
 		score = clf.score(X_test, Y_test)
 		print(f"{num_layers} layers, {layer_size} layer_size. Acc: {score}")
 '''
-
-'''
-#Using Keras, LSTMS and GRUs
-
 
